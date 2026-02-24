8e9343a856ad8ae255a31dabd1ab9acc5c3cca79
SonOfDadKiller <73202108+SonOfDadKiller@users.noreply.github.com>
2026-02-24 14:26:09 +1000

    added loading widget to login page

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -7,21 +7,28 @@ export default function LoginPage() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState('');
+    const [loading, setLoading] = useState(false);
     const { login, register } = useAuth();
     const navigate = useNavigate();
 
     const handleSubmit = async (e: React.FormEvent) => {
+        console.log('submit');
         e.preventDefault();
         setError('');
 
         try {
+            setLoading(true);
             if (isRegistering) {
                 await register(email, password);
             } else {
                 await login(email, password);
             }
+            console.log('Login Succeeded');
+            setLoading(false);
             navigate('/dashboard');
         } catch (err: any) {
+            console.log('Login Failed');
+            setLoading(false);
             setError(err.response?.data || 'Something went wrong');
         }
     };
@@ -54,9 +61,13 @@ export default function LoginPage() {
 
                         {error && <p className="error">{error}</p>}
 
-                        <button type="submit" className='lower-margin full-width filled'>
-                            {isRegistering ? 'Register' : 'Log In'}
+                        <button type="submit" disabled={loading} className='lower-margin full-width filled loader-parent'>
+                            <div className='loader-text'>
+                                {isRegistering ? 'Register' : 'Log In'}
+                            </div>
+                            <div className={`loader ${loading ? 'loader-reveal' : ''}`}></div>
                         </button>
+
                     </form>
 
                     <button className='full-width' onClick={() => setIsRegistering(!isRegistering)}>
